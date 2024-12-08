528ca580f00c8fad871baeba8602f0644aafa08b
Rae-An Andres <raeandres@users.noreply.github.com>
2024-12-08 22:49:53 +0800

    Implementation to main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,29 @@
 package org.example
 
-fun main() {
-    println("Hello World!")
+import com.google.crypto.tink.hybrid.HybridConfig
+import org.kohsuke.args4j.CmdLineException
+import org.kohsuke.args4j.CmdLineParser
+import java.security.GeneralSecurityException
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+
+    HybridConfig.register()
+
+    val keyUtil = KeyUtil()
+    val parser = CmdLineParser(keyUtil)
+
+    try {
+        parser.parseArgument(*args)
+    } catch (e: CmdLineException) {
+        println(e)
+        e.parser.printUsage(System.out)
+        exitProcess(1)
+    }
+    try {
+        keyUtil.keyCommand!!.run()
+    } catch (e: GeneralSecurityException) {
+        println("Cannot encrypt or decrypt, got error: $e")
+        exitProcess(1)
+    }
 }
